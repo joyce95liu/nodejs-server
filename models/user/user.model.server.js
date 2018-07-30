da58604a66d963c2b146e246dca18c449619eacb
jiayiliu <liu.jiayi@husky.neu.edu>
2018-07-30 07:26:47 -0700

    modified

--- models/user/user.model.server.js
+++ models/user/user.model.server.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var userSchema = require('./user.schema.server');
 
-var userModel = mongoose.model('UserModel', userSchema)
+var userModel = mongoose.model('UserModel', userSchema);
 
 function findUserById(userId) {
     return userModel.findById(userId);
